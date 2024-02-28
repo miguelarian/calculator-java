ece0cb810344dcdf7e9dc99a5979c084cf9d3909
miguelarian <miguelvr88@gmail.com>
2024-02-28 11:41:41 +0100

    rename operation parameters to have meaningful names

--- Calculator/src/com/miguelvela/calculator/Calculator.java
+++ Calculator/src/com/miguelvela/calculator/Calculator.java
@@ -1,16 +1,16 @@
 package com.miguelvela.calculator;
 
 public class Calculator {
-    public double sum(double a, double b) {
-        return a + b;
+    public double sum(double operatorA, double operatorB) {
+        return operatorA + operatorB;
     }
-    public double sub(double a, double b) {
-        return a - b;
+    public double sub(double operatorA, double operatorB) {
+        return operatorA - operatorB;
     }
-    public double divide(double a, double b) {
-        return a / b;
+    public double divide(double operatorA, double operatorB) {
+        return operatorA / operatorB;
     }
-    public double multiply(double a, double b) {
-        return a * b;
+    public double multiply(double operatorA, double operatorB) {
+        return operatorA * operatorB;
     }
 }
